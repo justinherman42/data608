36214a402d882a79130ba18e014c291dcb7f82cf
Justin Herman <justinherman42@gmail.com>
2019-11-10 22:06:17 -0500

    Delete president.js

--- apps_module_5/president.js
+++
@@ -1,9 +0,0 @@
-d3.csv('presidents.csv')
-  .then(function(data) {
-  	console.log(data);
-      // data is now whole data set
-      // draw chart in here!
-  })
-  .catch(function(error){
-     // handle error   
-  })
